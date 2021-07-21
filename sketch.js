bce2d1ff34fd01689605c96dabd4841c4bb2fa02
rajveerwh <86005759+rajveerwh@users.noreply.github.com>
2021-07-21 12:58:09 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,66 +1,34 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+var car,wall,speed,weight;
 
-function preload()
-{
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
-}
+
+
+
 
 function setup() {
-	createCanvas(800, 700);
-	rectMode(CENTER);
-	
-
-	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
-
-	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
-
-	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
-
-
-	engine = Engine.create();
-	world = engine.world;
-
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
-	World.add(world, packageBody);
-	
-
-	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
-
-
-	Engine.run(engine);
-  
+  createCanvas(1600,400);
+ car=createSprite(50, 200, 50, 50);
+ wall=createSprite(800,200,60,height/2);
+ wall.shapeColor="blue";
+  speed=random(50,90);
+  weight=random(400,1500);
+  car.velocityX= speed;
 }
-
 
 function draw() {
-  rectMode(CENTER);
-  background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
+
+  background("black"); 
+  if(wall.x-car.x<(car.width+wall.width)/2){
+    car.velocityX=0;
+    var deformation=0.5*weight*speed*speed/22500;
+    if(deformation<100){
+      car.shapeColor="green";
+    }
+    else if(deformation>180){
+      car.shapeColor="red";
+    }
+    else if(deformation>100&&deformation<180){
+      car.shapeColor="yellow";
+    }
+  } 
   drawSprites();
- 
 }
-
-function keyPressed() {
- if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
-Matter.Body.setStatic(packageBody,false);
-    
-  }
-}
-
-
-
